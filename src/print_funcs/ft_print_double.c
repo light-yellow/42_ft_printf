7bf22673d9f4b6ff8520c3fcc5536cf453eebb2f
Ben-Dudley <vilenaguslyakova@gmail.com>
2019-05-10 17:27:08 +0300

    flags double

--- src/print_funcs/ft_print_double.c
+++ src/print_funcs/ft_print_double.c
@@ -20,27 +20,25 @@ static char	*ft_put_double(long double value, t_format *f)
 	int		counter;
 	int     i;
 
-	integer = ft_itoa_base(value, 10, 'a'); //ch delat
-//	counter = ft_strlen(integer) + (value > 0) ? counter : counter + 1;
-//    counter = ((f->precision + counter) > f->min_width) ? f->precision + counter : f->min_width;
-	fractional = ft_strnew(f->precision + 1);
 	value = value > 0 ? value : -value;
-	value -= (int)value;
+	integer = ft_ulltoa_base((unsigned long long)value, 10, 'a');
+	fractional = ft_strnew(f->precision + 1);
+	value -= (unsigned long long)value;
 	if ((f->precision == -1 && f->hash == 1) || f->precision != -1)
 	    fractional[0] = '.';
 	counter = 1;
     while (counter <= f->precision)
 	{
-		if (counter == f->precision && ((int)(value * 100) % 10) > 4)
+		if (counter == f->precision && ((unsigned long long)(value * 100) % 10) > 4)
 			value += 0.1;
-		fractional[counter] = (int)(value * 10) + '0';
+		fractional[counter] = (unsigned long long)(value * 10) + '0';
 		i = 0;
 		while (fractional[counter - i] > 57)
         {
 		    fractional[counter - i] = '0';
 		    fractional[counter - ++i]++;
         }
-		value = value * 10 - (int)(value * 10);
+		value = value * 10 - (unsigned long long)(value * 10);
 		counter++;
 	}
 	str = ft_strnew(ft_strlen(integer) + ft_strlen(fractional));
@@ -58,27 +56,50 @@ void			ft_print_double(char **str, t_format *f)
 	char	*ptr;
 	char    *s;
 
+	s = NULL;
 	f->precision = (f->precision == 0) ? 6 :f->precision;
 	if (f->length == 7)
 	    value = va_arg(f->ap, long double);
 	else
         value = va_arg(f->ap, double);
 	ptr = ft_put_double(value, f);
-    counter = (f->plus == 1) ? ft_strlen(ptr) + 1 : ft_strlen(ptr);
+    counter = ((value >= 0 && f->plus == 1)|| value < 0) ? ft_strlen(ptr) + 1 : ft_strlen(ptr);
 	if (counter < f->min_width)
     {
 	    s = ft_strnew(f->min_width - counter);
 	    if (f->zero == 0)
-	        s = (char *)ft_memset((void *)s, ' ', f->min_width - counter);
-//	    else
-  //          s = (char *)ft_memset((void *)s, '0', f->min_width - counter);
-        ft_putbuffer(f, s, ft_strlen(s));
-	    free(s);
+			s = (char *)ft_memset((void *)s, ' ', f->min_width - counter);
+	    else
+		{
+			if (f->space == 1 && f->plus == 0 && value >= 0)
+			{
+				ft_putbuffer(f, " ", 1);
+				counter++;
+			}
+			s = (char *)ft_memset((void *)s, '0', f->min_width - counter);
+			if (value >= 0 && f->plus == 1)
+				ft_putbuffer(f, "+", 1);
+			else if (value < 0)
+				ft_putbuffer(f, "-", 1);
+		}
+	    if (f->minus != 1)
+		{
+			ft_putbuffer(f, s, ft_strlen(s));
+			free(s);
+		}
+
     }
-	else if (f->space == 1 && f->plus == 0 && f->minus == 0)
+	else if (f->space == 1 && f->plus == 0 && value >= 0)
         ft_putbuffer(f, " ", 1);
-	if (f->plus == 1 && ptr[0] != '-')
-        ft_putbuffer(f, "+", 1);
-    ft_putbuffer(f, ptr, ft_strlen(ptr));
+	if (value >= 0 && f->plus == 1 && (f->zero == 0 || counter >= f->min_width))
+		ft_putbuffer(f, "+", 1);
+	else if (value < 0 && (f->zero == 0 || counter >= f->min_width))
+		ft_putbuffer(f, "-", 1);
+	ft_putbuffer(f, ptr, ft_strlen(ptr));
+	if (f->minus == 1 && s)
+	{
+		ft_putbuffer(f, s, ft_strlen(s));
+		free(s);
+	}
 	*str += 1;
 }
